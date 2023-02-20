dceb28c165831b4471013b78ab16074613fa3dc5
Parth Vohra <72933441+parthvohra25@users.noreply.github.com>
2023-02-20 11:08:08 +0530

    Create Smiley.cpp

---
+++ Smiley.cpp
@@ -0,0 +1,128 @@
+#include <GL/glut.h>
+#include <stdio.h>
+using namespace std;
+
+int pntX1 = 250, pntY1 = 250, r = 50;
+void plot(int x, int y)
+{
+    glBegin(GL_POINTS);
+    glVertex2i(x + pntX1, y + pntY1);
+    glEnd();
+}
+void midPointCircleAlgo()
+{
+    int x = 0;
+    int y = r;
+    float decision = 5 / 4 - r;
+    plot(x, y);
+    while (y > x)
+    {
+        if (decision < 0)
+        {
+            x++;
+            decision += 2 * x + 1;
+        }
+        else
+        {
+            y--;
+            x++;
+            decision += 2 * (x - y) + 1;
+        }
+        plot(x, y);
+        plot(x, -y);
+        plot(-x, y);
+        plot(-x, -y);
+        plot(y, x);
+        plot(-y, x);
+        plot(y, -x);
+        plot(-y, -x);
+    }
+}
+int x1, y1, x2, y2;
+void myInit()
+{
+	glPointSize(1.0);
+	glLoadIdentity();
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(0, 500, 0, 500);
+}
+void draw_pixel(int x, int y)
+{
+	glBegin(GL_POINTS);
+	glVertex2i(x, y);
+	glEnd();
+}
+void draw_line(int x1, int x2, int y1, int y2) {
+	int dx, dy, i, e;
+	int incx, incy, inc1, inc2;
+	int x, y;
+	dx = x2 - x1;
+	dy = y2 - y1;
+	if (dx < 0) dx = -dx;
+	if (dy < 0) dy = -dy;
+	incx = 1;
+	if (x2 < x1) incx = -1;
+	incy = 1;
+	if (y2 < y1) incy = -1;
+	x = x1; y = y1;
+	if (dx > dy) {
+		draw_pixel(x, y);
+		e = 2 * dy - dx;
+		inc1 = 2 * (dy - dx);
+		inc2 = 2 * dy;
+		for (i = 0; i < dx; i++)
+		{
+			if (e >= 0)
+			{
+				y += incy;
+				e += inc1;
+			}
+			else
+				e += inc2;
+			x += incx;
+			draw_pixel(x, y);
+		}
+	}
+	else
+	{
+		draw_pixel(x, y);
+		e = 2 * dx - dy;
+		inc1 = 2 * (dx - dy);
+		inc2 = 2 * dx;
+		for (i = 0; i < dy; i++)
+		{
+			if (e >= 0)
+			{
+				x += incx;
+				e += inc1;
+			}
+			else
+				e += inc2;
+			y += incy;
+			draw_pixel(x, y);
+		}
+	}
+}
+void myDisplay()
+{
+	draw_line(250, 250, 250, 260);
+	draw_pixel(220, 260);
+	draw_pixel(270, 260);
+	midPointCircleAlgo();
+	draw_line(230, 250, 220, 220);
+	glFlush();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("ice cream");
+	myInit();
+	glutDisplayFunc(myDisplay);
+	glutMainLoop();
+}
